069e84d69bee4ced888579df1071434fd2557e10
Ethan Rubio <ethanrubio@gmail.com>
2016-06-04 11:06:42 -0700

    [bug] fixes merge conflict leftovers

--- server/controllers/crimeScore.controller.js
+++ server/controllers/crimeScore.controller.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-const request = require('request')
-
-
-module.exports = (req,res,next) =>{
-
-	request("https://jgentes-Crime-Data-v1.p.mashape.com/crime")
-
-}
-=======
 const crimeData = require('../models/model.crimeData.js')
 
 module.exports = (app,express) => {
 
 }
 
->>>>>>> crime
+
