1c6cce03387b017052f369511af7b3960dec72f4
Ethan Rubio <ethanrubio@gmail.com>
2016-06-04 11:13:25 -0700

    [chore] cleans up white space

--- server/models/model.crimeData.js
+++ server/models/model.crimeData.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 var crimes = {}
 
-request(crimeConfig.crimeData, (req, res)=>{
-	for(var i = 0; i < res.body.length; i++){
-		if(!crimes[res.body[i].city]){
-			crimes[res.body[i].city] = []
-			crimes[res.body[i].city].push(res.body[i])
+request(crimeConfig.crimeData, (req, res) => {
+	for (var i = 0; i < res.body.length; i++) {
+		if (!crimes[res.body[i].city]) {
+			crimes[res.body[i].city] = [];
+			crimes[res.body[i].city].push(res.body[i]);
 		} else {
-			crimes[res.body[i].city].push(res.body[i])
+			crimes[res.body[i].city].push(res.body[i]);
 		}
 	}
 })
 
-module.exports = crimes
+module.exports = crimes;
 
