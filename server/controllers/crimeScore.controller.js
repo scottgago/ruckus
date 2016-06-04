5f9ab23d5c7669bfe02c53a343b85a88565d0901
scottgago <scottgago@gmail.com>
2016-06-04 14:53:57 -0700

    fixed route pathing

--- server/controllers/crimeScore.controller.js
+++ server/controllers/crimeScore.controller.js
@@ -1,15 +1,19 @@
-const crimeData = require('../models/model.crimeData.js');
-const yelpAPI = require('../lib/yelpAPI.js');
+const crimeData     = require('../models/model.crimeData.js');
+const yelpAPI       = require('../lib/yelpAPI.js');
 const request       = require('request')
 const configHeaders = require('../config/config.crimeData.js')
 
 module.exports = {
-	getCrimeAroundBars: (req, res) => {
+	getCrime: (req, res) => {
 		configHeaders.url = configHeaders.url + '?$where=within_circle(geo_crime_location, ' + req.body.latitude + ', ' + req.body.longitude + ', 27300)'
 
 		request(configHeaders, (err, data) => {
 			res.json(data)
 		})
 		
+	},
+	getBars: (req,res) => {
+		var latLongString = req.body.latitude + "," + req.body.longitutde
+		yelpAPI(latLongString) 
 	}
 };
