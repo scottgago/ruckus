4d85e137ab226aa55af58df51311bf4522779948
Ethan Rubio <ethanrubio@gmail.com>
2016-06-04 11:10:12 -0700

    [refactor] moves requires outside of module

--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,10 @@
 const bodyParser = require('body-parser');
+const crimeScoreController = require('../controllers/crimeScore.controller.js');
 
 module.exports = (app, express) => {
 	//register bodyparsing middleware
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
-	//register crimeScore controller
-	require('../controllers/crimeScore.controller.js')(app, express);
-	
-	app.post('/api');
+	//register crimeScore controller	
+	app.post('/api', crimeScoreController);
 }
